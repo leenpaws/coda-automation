486377f42f6d40dc23e47da07f9245733f56b40d
hashquark-research <wangzeshu@wancloud.io>
2019-10-22 10:08:53 +0800

    Staking Challenge: Adding Key for hashquark#8413

--- scripts/testnet-tools/pubkey_to_discord.py
+++ scripts/testnet-tools/pubkey_to_discord.py
@@ -38,6 +38,7 @@ STAKING_CHALLENGE = {
     'tdNE4ogXK7oM1QSUiueUQPcYkawNqyMekjmzg8azroWAPLbZAANxLBrYtYMHFVqjFvXNiVSbdnjzGZxixeePdgaALXQcNPhEznMzGgmm1geqynU9Hyxj6gd3icDJet3wHDBLCKsgGVQFFe': 'fullmoon#9069',
     'tdNEAq5uer5nTv9VYkE2YDJLxXUas342xjxdaXjEYUZfzGoNwxqv1rEUBdhRHgTyh5PUHjq7UstsWPNtSynUqFukxzdUkWa5Q1QpiCxV8vW7d3hHvbiegcGZHFiwR3mnN4Q5yAUfCPMEGB': 'RomanS#8785'
     'tdNDowo9QyHT1KnkQP4173GMona8MdrQ7UbG1E1BxdUgyxAMh9KK2w9tfcBcfxWgQzH29JtZpFZ7MekRtytfR5no6RjdnKdgrx5PByfEXhtZzX3HX74qVEL33qnDfdf6gqMpCxjjgLHECY': 'niuniu#9001'
+    'tdNEBeQAX4osSqGW99hvKcqwpWAAq7Q8wTTy68cqHutVFKdmL3jDb7dry3wcxphosgLTNFBbJNv3d1ffncSAZ9m8xiVv1sfV2mUYREZHN5MNbgVNCpn55me3mzvRBrerqxosyzoVkoHFKW': 'hashquark#8413'
 }
 
 # For Community Members
