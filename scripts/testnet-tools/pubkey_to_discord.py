93018206c6cc39d929df9fb56b8014010fc25457
Kareem Moussa <itstehkman@gmail.com>
2019-12-07 14:33:29 -0800

    [chore] add karmoussa95 pubkey

--- scripts/testnet-tools/pubkey_to_discord.py
+++ scripts/testnet-tools/pubkey_to_discord.py
@@ -2,7 +2,7 @@
 # Example:
 #   'tNciczxpMfZ4eW1ZPP9NVK2vxcm9cCHvTBWMe8Nskn2A25P1YqcdvFCD5LvgngraiCmAsnC8zWAiv5pwMYjrUwpMNYDePMQYiXX7HVMjrnB1JkEckyayvsAm2Bo4EQBWbHXD5Cxp65PZy5': 'DISCORD_USERNAME#1234',
 STAKING_CHALLENGE = {
-    'tdNDgeZGYU5MppfvYpBULVPagefvgPVip6WEqaipqwfd62yKmWVR9NjUkDVoFMTtWzbnX4QEPKLNv8z86G5xAMC1zDWRFk2266WCKNoTVnnDjosWj1CJBFAbueazAMpCsaVzLHwbpgh2ws': 
+    'tdNDgeZGYU5MppfvYpBULVPagefvgPVip6WEqaipqwfd62yKmWVR9NjUkDVoFMTtWzbnX4QEPKLNv8z86G5xAMC1zDWRFk2266WCKNoTVnnDjosWj1CJBFAbueazAMpCsaVzLHwbpgh2ws':
     'WilliamGuozi#6724 | Sparkpool',
     'tdNDfH9oVR4AoapbuC7fFewMKN8hKMbhF7WhhUTQrpAR2gxbv6fqGiM68Qpk8soQVViX7RzoPETfi1xN2hLbazXhP5qczGyiasMVVWJzc6t3EF5NusvY2vWs91KjjPhZewS71LPRcotxzh':
     'Star.LI#0785',
@@ -493,7 +493,9 @@ OBSERVED_COMMUNITY_PUBLIC_KEYS = {
     'tNcikquYryJPeCTEFEtUj85rdZkuLsH5QJV1kbY1xJQr6RcrHSHMtAKqbZ3Xz5rCzTXLzaJvixco1V4zgZJNPxArSmD783dhs9zaB8U1txc8LPGf5ecyLuB3Z3vR3CbETrCaEGN4ANEyWt':
     'novy',
     'tNcimm3CnFATS2WUL9WJuGv8nXzsRTLCQCcuCbAAoixT5KeA8cUhTkddcyEBikG4baP3MMUMgTxm2R2L9C3HMcWA626qrRDZXgFVX5nvZeFPH7WcMdVmN9tSrvrLB6YPig3XHeKV7Hu9rP':
-    'Alexander'
+    'Alexander',
+    'tdNE5uAzFmcwxFR9XFVqrEu69ymkapEFwosHKeioxQFzuzkxF7jFdDBCJhennaiHctiin2bNfC8MSXCKSRMBpveJpGVzWwNrjsqe6EXf6eViqrVVu5VNiPsCgBmH6JcW6eBWxjQaubM2Yo':
+    'karmoussa95#6768'
 }
 
 # For O(1) Staff
